122df4f42d67a6e48d65997b532c99f9de8edc2e
On <40337137+on-sirtk@users.noreply.github.com>
2019-09-10 12:06:56 +0800

    Create 08_multiIf.py

---
+++ 08_multiIf.py
@@ -0,0 +1,14 @@
+# 由使用者寫輸入 m 或 f , 決定他是男或女 
+sex = input("你是 M 還是 F")
+
+# 条件：如果你拥有的宝石数量大于等于6个
+if sex == "M" or sex == "m":
+  print('你是男人')
+    
+elif sex == "F" or sex == "f":
+   print ('你是女人')
+   
+else :
+   print ('你不男不女')
+   
+   
