063ebcc569ceb355a82465d114adfac2bbedb7bb
On <40337137+on-sirtk@users.noreply.github.com>
2019-09-11 11:19:33 +0800

    Update 10_function.py

--- 10_function.py
+++ 10_function.py
@@ -4,11 +4,11 @@ num1 = input("第一個數?")
 num2 = input("第二個數?")
 
 #測試正整數
-if (check(num1) and check(num2)):
+if (lcm.check(num1) and lcm.check(num2)):
   num1 = int(num1)
   num2 = int(num2)
   #** 加入你的代碼 **
   
   
   
-  lcm(num1,num2)
+  lcm.lcm(num1,num2)
